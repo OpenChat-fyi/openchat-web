ed315f760ede18f00b627a1dec3e34b2c5261bf6
orbiter <goge.egog@mail.ru>
2023-11-27 22:12:17 +0300

    remove EIP6963 provider and legacy code from web3

--- src/context/web3-provider.tsx
+++ src/context/web3-provider.tsx
@@ -1,39 +1,3 @@
-// "use client";
-
-// import { createWeb3Modal, defaultWagmiConfig, useWeb3Modal, useWeb3ModalTheme } from '@web3modal/wagmi/react'
-
-// import { WagmiConfig } from 'wagmi'
-// import { optimismGoerli, sepolia, arbitrumGoerli } from 'viem/chains'
-
-// // 1. Get projectId at https://cloud.walletconnect.com
-// const projectId = '51afd8a7bd88ae02dbbbfa6ee148a697'
-
-// // 2. Create wagmiConfig
-// const metadata = {
-//   name: 'Web3Modal',
-//   description: 'Web3Modal Example',
-//   url: 'https://web3modal.com',
-//   icons: ['https://avatars.githubusercontent.com/u/37784886']
-// }
-
-// const chains = [optimismGoerli, arbitrumGoerli, sepolia]
-// const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
-
-// // 3. Create modal
-// createWeb3Modal({ 
-//   wagmiConfig, 
-//   projectId, 
-//   chains, 
-//   themeMode: 'light',
-//   themeVariables: {
-//     '--w3m-border-radius-master': '1px'
-//   }
-// })
-
-// export function Web3Modal({ children }) {
-//   return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
-// }
-
 "use client";
 import { createWeb3Modal } from '@web3modal/wagmi/react'
 import { walletConnectProvider, EIP6963Connector } from '@web3modal/wagmi'
@@ -48,10 +12,8 @@ import { InjectedConnector } from 'wagmi/connectors/injected'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { ReactNode } from 'react';
 
-// 1. Get projectId at https://cloud.walletconnect.com
 const projectId = '51afd8a7bd88ae02dbbbfa6ee148a697'
 
-// 2. Create wagmiConfig
 const { chains, publicClient } = configureChains(
   [optimismGoerli, arbitrumGoerli, sepolia],
   [walletConnectProvider({ projectId }), publicProvider(), infuraProvider({ apiKey: 'b2b567b229984f4299f9ca3d211ebbdb' })]
@@ -68,7 +30,6 @@ const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: [
     new WalletConnectConnector({ chains, options: { projectId, showQrModal: false, metadata } }),
-    // new EIP6963Connector({ chains }),
     new InjectedConnector({ chains, options: { shimDisconnect: true } }),
     new CoinbaseWalletConnector({ chains, options: { appName: metadata.name } })
   ],
