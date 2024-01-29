58dd6f65a67ed5ff14aef944e0365ca1f12e014a
orbiter <lunareclipsewasshort@gmail.com>
2024-01-29 04:32:45 +0300

    feat: blasting update!

--- src/app/chat/chat-main.tsx
+++ src/app/chat/chat-main.tsx
@@ -1,281 +1,292 @@
 "use client";
-import dynamic from 'next/dynamic'
-import { Input } from '@/shadcn-components/ui/input';
-import { useAccount, useNetwork, usePrepareContractWrite, useContractWrite } from 'wagmi';
-import React, { useState, useEffect, useRef } from 'react'
-import { useChannel } from 'ably/react'
-import axios from 'axios';
+import dynamic from "next/dynamic";
+import { Input } from "@/shadcn-components/ui/input";
+import { useAccount, useNetwork, usePrepareContractWrite, useContractWrite } from "wagmi";
+import React, { useState, useEffect, useRef } from "react";
+import { useChannel } from "ably/react";
+import axios from "axios";
 import { useInfiniteQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { Message } from '@/types/custom';
-import { EmojiPicker } from '@/components/emoji-picker';
+import { Message } from "@/types/custom";
+import { EmojiPicker } from "@/components/emoji-picker";
 
-const MessageItem = dynamic(() => import('@/components/message-item'), { ssr: false });
-const MessageButton = dynamic(() => import('@/components/buttons').then((mod) => mod.MessageButton))
+const MessageItem = dynamic(() => import("@/components/message-item"), { ssr: false });
+const MessageButton = dynamic(() => import("@/components/buttons").then((mod) => mod.MessageButton));
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export type ChainList = "11155111" | "420" | "421613" | "59144" | "534352" | "8453"
+export type ChainList = "11155111" | "420" | "421613" | "59144" | "534352" | "8453" | "168587773";
 export const CHAIN_CONFIG = {
-  "11155111": { // eth sepolia
-      contract: '0x80fAb1fdE6C8216DE73112d207602930E118850b',
-      explorer: 'https://sepolia.etherscan.io/tx/'
-  },
-  "420": { // optimism goerli
-      contract: '0x5A0B048F1c5BCeFfccC04931F934634D3cF6C65b',
-      explorer: 'https://goerli-optimism.etherscan.io/tx/'
-  },
-  "421613": { // arbitrum goerli
-      contract: '0xa8036D3BB3f4ff91C5c92a14CBE2348Bb79435da',
-      explorer: 'https://goerli.arbiscan.io/tx/'
-  },
-  "59144": { // linea mainnet
-      contract: '0x4cBaF08033c65C9bE66A9cd4Ed26E96a028a5F8e',
-      explorer: 'https://lineascan.build/tx/'
-  },
-  "534352": { // scroll mainnet
-      contract: '0x52BAEF174b0a2758Bc78286464C624aCeb2cBa0c',
-      explorer: 'https://scrollscan.com/tx/'
-  },
-  "8453": {
-      contract: '0xEE028cbD99A675C1d19CCe4E5FD8d1Ef53701fe2',
-      explorer: 'https://basescan.org/tx/'
-  }
-}
+	"11155111": {
+		// eth sepolia
+		contract: "0x80fAb1fdE6C8216DE73112d207602930E118850b",
+		explorer: "https://sepolia.etherscan.io/tx/",
+	},
+	"420": {
+		// optimism goerli
+		contract: "0x5A0B048F1c5BCeFfccC04931F934634D3cF6C65b",
+		explorer: "https://goerli-optimism.etherscan.io/tx/",
+	},
+	"421613": {
+		// arbitrum goerli
+		contract: "0xa8036D3BB3f4ff91C5c92a14CBE2348Bb79435da",
+		explorer: "https://goerli.arbiscan.io/tx/",
+	},
+	"59144": {
+		// linea mainnet
+		contract: "0x4cBaF08033c65C9bE66A9cd4Ed26E96a028a5F8e",
+		explorer: "https://lineascan.build/tx/",
+	},
+	"534352": {
+		// scroll mainnet
+		contract: "0x52BAEF174b0a2758Bc78286464C624aCeb2cBa0c",
+		explorer: "https://scrollscan.com/tx/",
+	},
+	"8453": {
+		contract: "0xEE028cbD99A675C1d19CCe4E5FD8d1Ef53701fe2",
+		explorer: "https://basescan.org/tx/",
+	},
+	"168587773": {
+		contract: "0x8456b1b38DCefce2838CEed5f7C6052731C64851",
+		explorer: "https://testnet.blastscan.io",
+	},
+};
 const fetchMessages = async ({ pageParam }: { pageParam: number }) => {
-  const response = await axios.get(`${API_URL}/message?page=${pageParam}`, {
-    headers: {
-      "ngrok-skip-browser-warning": "69420"
-    }
-  });
-  return { messages: response.data, nextCursor: response.data.length ? response.data.length === 50 ? pageParam + 1 : undefined : undefined };
+	const response = await axios.get(`${API_URL}/message?page=${pageParam}`, {
+		headers: {
+			"ngrok-skip-browser-warning": "69420",
+		},
+	});
+	return {
+		messages: response.data,
+		nextCursor: response.data.length ? (response.data.length === 50 ? pageParam + 1 : undefined) : undefined,
+	};
 };
 const sendMessageToServer = async (message: Message) => {
-  const response = await axios.post(`${API_URL}/message`, message, {
-    headers: {
-      "ngrok-skip-browser-warning": "69420"
-    }
-  });
-  return response.data;
+	const response = await axios.post(`${API_URL}/message`, message, {
+		headers: {
+			"ngrok-skip-browser-warning": "69420",
+		},
+	});
+	return response.data;
 };
 
 export default function ChatMain() {
-  const { address } = useAccount()
-  const { chain } = useNetwork()
-  const { config } = usePrepareContractWrite({
-    address: CHAIN_CONFIG[chain?.id?.toString() as ChainList]?.contract as `0x{string}`,
-    abi: [
-      {
-        name: 'message',
-        type: 'function',
-        stateMutability: 'nonpayable',
-        inputs: [],
-        outputs: [],
-      },
-    ],
-    functionName: 'message',
-  })
-  const { data: write_data, isLoading: write_isLoading, isSuccess: write_isSuccess, isError: write_isError, write } = useContractWrite(config)
-  const queryClient = useQueryClient()
-  const { channel } = useChannel("openchat:messages", (event) => {
-    console.log(event);
-    const message: Message = event.data
-    if (message.sender !== address?.toLowerCase()) {
-      queryClient.setQueryData(['messages'], (old: any) => {
-        const newPages = old.pages.map((page: any, index: number) => {
-          // Only update the last page
-          if (index === old.pages.length - 1) {
-            return {
-              ...page,
-              messages: [message, ...page.messages],
-            };
-          } else {
-            return page;
-          }
-        });
-      
-        return {
-          ...old,
-          pages: newPages,
-        };
-      });
-    }
-  });
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-  } = useInfiniteQuery({
-      queryKey: ['messages'], 
-      queryFn: fetchMessages, 
-      initialPageParam: 1,
-      getNextPageParam: (lastPage, pages) => lastPage.nextCursor,
-    });
-  const { mutate } = useMutation({
-    mutationKey: ['addMessage'],
-    mutationFn: sendMessageToServer,
-    // Optimistic update
-    onMutate: async (newMessage: Message) => {
-      // Cancel any outgoing refetches
-      await queryClient.cancelQueries({ queryKey: ['messages'] });
-  
-      // Snapshot the previous value
-      const previousMessages = queryClient.getQueryData(['messages']);
+	const { address } = useAccount();
+	const { chain } = useNetwork();
+	const { config } = usePrepareContractWrite({
+		address: CHAIN_CONFIG[chain?.id?.toString() as ChainList]?.contract as `0x{string}`,
+		abi: [
+			{
+				name: "message",
+				type: "function",
+				stateMutability: "nonpayable",
+				inputs: [],
+				outputs: [],
+			},
+		],
+		functionName: "message",
+	});
+	const {
+		data: write_data,
+		isLoading: write_isLoading,
+		isSuccess: write_isSuccess,
+		isError: write_isError,
+		write,
+	} = useContractWrite(config);
+	const queryClient = useQueryClient();
+	const { channel } = useChannel("openchat:messages", (event) => {
+		console.log(event);
+		const message: Message = event.data;
+		if (message.sender !== address?.toLowerCase()) {
+			queryClient.setQueryData(["messages"], (old: any) => {
+				const newPages = old.pages.map((page: any, index: number) => {
+					// Only update the last page
+					if (index === old.pages.length - 1) {
+						return {
+							...page,
+							messages: [message, ...page.messages],
+						};
+					} else {
+						return page;
+					}
+				});
 
-      console.log(previousMessages)
+				return {
+					...old,
+					pages: newPages,
+				};
+			});
+		}
+	});
+	const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
+		queryKey: ["messages"],
+		queryFn: fetchMessages,
+		initialPageParam: 1,
+		getNextPageParam: (lastPage, pages) => lastPage.nextCursor,
+	});
+	const { mutate } = useMutation({
+		mutationKey: ["addMessage"],
+		mutationFn: sendMessageToServer,
+		// Optimistic update
+		onMutate: async (newMessage: Message) => {
+			// Cancel any outgoing refetches
+			await queryClient.cancelQueries({ queryKey: ["messages"] });
 
-      queryClient.setQueryData(['messages'], (old: any) => {
-        const newPages = old.pages.map((page: any, index: number) => {
-          // Only update the last page
-          if (index === old.pages.length - 1) {
-            console.log('yolo')
-            return {
-              ...page,
-              messages: [newMessage, ...page.messages],
-            };
-          } else {
-            return page;
-          }
-        });
-      
-        return {
-          ...old,
-          pages: newPages,
-        };
-      });
-  
-      // Return a context object with the snapshotted value
-      return { previousMessages };
-    },
-    // If the mutation fails, roll back to the previous value
-    onError: (err, newMessage, context) => {
-      if (context?.previousMessages) {
-        queryClient.setQueryData(['messages'], context.previousMessages);
-      }
-    },
-    // Always refetch after error or success:
-    onSettled: (data, error, variables, context) => {
-      const newMessage = { ...variables, isLoading: false };
-      queryClient.setQueryData(['messages'], (old: any) => {
-        const newPages = old.pages.map((page: any, index: number) => {
-          if (index === old.pages.length - 1) {
-            return {
-              ...page,
-              messages: page.messages.map((message: Message) => message.id === newMessage.id ? newMessage : message),
-            };
-          } else {
-            return page;
-          }
-        });
-    
-        return {
-          ...old,
-          pages: newPages,
-        };
-      });
-      queryClient.invalidateQueries({ queryKey: ['messages'] });
-    },
-  });
+			// Snapshot the previous value
+			const previousMessages = queryClient.getQueryData(["messages"]);
 
-  const [input, setInput] = useState('');
-  const messagesEndRef = useRef<HTMLDivElement | null>(null);
-  const loadMoreRef = useRef<HTMLDivElement | null>(null);
+			console.log(previousMessages);
 
-  useEffect(() => {
-    if (write_isSuccess) {
-      const newMessage: Message = {
-        id: write_data?.hash || '',
-        sender: address || '', 
-        message: input!, 
-        time_data: { iat: new Date() }, 
-        chain: { 
-          name: chain?.name || '', 
-          id: chain?.id.toString() || ''
-        },
-        isLoading: true,
-      };
-  
-      mutate(newMessage);
-      setInput('');
-    }
-  }, [write_isSuccess]);
-  useEffect(() => {
-    if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [data]);
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
-          fetchNextPage();
-        }
-      },
-      { threshold: 1 }
-    );
-  
-    if (loadMoreRef.current) {
-      observer.observe(loadMoreRef.current);
-    }
-  
-    return () => {
-      if (loadMoreRef.current) {
-        observer.unobserve(loadMoreRef.current);
-      }
-    };
-  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
+			queryClient.setQueryData(["messages"], (old: any) => {
+				const newPages = old.pages.map((page: any, index: number) => {
+					// Only update the last page
+					if (index === old.pages.length - 1) {
+						console.log("yolo");
+						return {
+							...page,
+							messages: [newMessage, ...page.messages],
+						};
+					} else {
+						return page;
+					}
+				});
 
-  function _sendMessage () { write?.() }  
+				return {
+					...old,
+					pages: newPages,
+				};
+			});
 
-  return (
-    <main className="flex flex-col min-h-screen items-center justify-between px-4 md:px-72 py-8 pt-11 max-h-screen">
-      <div className='w-full h-[80vh] overflow-auto flex flex-col-reverse'>
-        <div className='space-y-2 flex flex-col-reverse'>
-        <div ref={messagesEndRef} />
-        {data?.pages.flatMap((group, i) => (
-          <React.Fragment key={i}>
-            {group.messages.map((message: Message, index: number) => (
-              <div className='my-2' key={index}>
-                <MessageItem message={message} />
-              </div>
-            ))}
-          </React.Fragment>
-        ))}
-        <div ref={loadMoreRef} />
-        <div className='h-16'/>
-      </div>
-      </div>
-      <div className="w-full pt-2">
-        <div className="grid w-full gap-2">
-          <div className="w-full flex justify-between space-x-2 pt-2 items-center">
-            <Input 
-              className={`${input.length > 256 ? 'border-destructive' : ''} max-sm:h-12`}
-              placeholder="Type your message here." 
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              onKeyPress={(e) => {
-                if (e.key === 'Enter') {
-                  if (e.shiftKey) {
-                    e.preventDefault();
-                    setTimeout(() => setInput(input + '\n'), 0);
-                  } else {
-                    // Send the message
-                    e.preventDefault();
-                    _sendMessage()
-                  }
-                }
-              }}
-            />
-            <EmojiPicker input={input} setInput={setInput} />
-          </div>
-          { input.length > 256 ? (
-              <p className="text-sm text-destructive">
-                Message must be under 256 characters
-              </p>
-            ) : null}
-          <MessageButton sendMessage={_sendMessage} disabled={input.length === 0 || write_isLoading}/>
-        </div>
-      </div>
-    </main>
-  )
+			// Return a context object with the snapshotted value
+			return { previousMessages };
+		},
+		// If the mutation fails, roll back to the previous value
+		onError: (err, newMessage, context) => {
+			if (context?.previousMessages) {
+				queryClient.setQueryData(["messages"], context.previousMessages);
+			}
+		},
+		// Always refetch after error or success:
+		onSettled: (data, error, variables, context) => {
+			const newMessage = { ...variables, isLoading: false };
+			queryClient.setQueryData(["messages"], (old: any) => {
+				const newPages = old.pages.map((page: any, index: number) => {
+					if (index === old.pages.length - 1) {
+						return {
+							...page,
+							messages: page.messages.map((message: Message) => (message.id === newMessage.id ? newMessage : message)),
+						};
+					} else {
+						return page;
+					}
+				});
+
+				return {
+					...old,
+					pages: newPages,
+				};
+			});
+			queryClient.invalidateQueries({ queryKey: ["messages"] });
+		},
+	});
+
+	const [input, setInput] = useState("");
+	const messagesEndRef = useRef<HTMLDivElement | null>(null);
+	const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		if (write_isSuccess) {
+			const newMessage: Message = {
+				id: write_data?.hash || "",
+				sender: address || "",
+				message: input!,
+				time_data: { iat: new Date() },
+				chain: {
+					name: chain?.name || "",
+					id: chain?.id.toString() || "",
+				},
+				isLoading: true,
+			};
+
+			mutate(newMessage);
+			setInput("");
+		}
+	}, [write_isSuccess]);
+	useEffect(() => {
+		if (messagesEndRef.current) {
+			messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+		}
+	}, [data]);
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
+					fetchNextPage();
+				}
+			},
+			{ threshold: 1 }
+		);
+
+		if (loadMoreRef.current) {
+			observer.observe(loadMoreRef.current);
+		}
+
+		return () => {
+			if (loadMoreRef.current) {
+				observer.unobserve(loadMoreRef.current);
+			}
+		};
+	}, [hasNextPage, isFetchingNextPage, fetchNextPage]);
+
+	function _sendMessage() {
+		write?.();
+	}
+
+	return (
+		<main className="flex flex-col min-h-screen items-center justify-between px-4 md:px-72 py-8 pt-11 max-h-screen">
+			<div className="w-full h-[80vh] overflow-auto flex flex-col-reverse">
+				<div className="space-y-2 flex flex-col-reverse">
+					<div ref={messagesEndRef} />
+					{data?.pages.flatMap((group, i) => (
+						<React.Fragment key={i}>
+							{group.messages.map((message: Message, index: number) => (
+								<div className="my-2" key={index}>
+									<MessageItem message={message} />
+								</div>
+							))}
+						</React.Fragment>
+					))}
+					<div ref={loadMoreRef} />
+					<div className="h-16" />
+				</div>
+			</div>
+			<div className="w-full pt-2">
+				<div className="grid w-full gap-2">
+					<div className="w-full flex justify-between space-x-2 pt-2 items-center">
+						<Input
+							className={`${input.length > 256 ? "border-destructive" : ""} max-sm:h-12`}
+							placeholder="Type your message here."
+							value={input}
+							onChange={(e) => setInput(e.target.value)}
+							onKeyPress={(e) => {
+								if (e.key === "Enter") {
+									if (e.shiftKey) {
+										e.preventDefault();
+										setTimeout(() => setInput(input + "\n"), 0);
+									} else {
+										// Send the message
+										e.preventDefault();
+										_sendMessage();
+									}
+								}
+							}}
+						/>
+						<EmojiPicker input={input} setInput={setInput} />
+					</div>
+					{input.length > 256 ? <p className="text-sm text-destructive">Message must be under 256 characters</p> : null}
+					<MessageButton sendMessage={_sendMessage} disabled={input.length === 0 || write_isLoading} />
+				</div>
+			</div>
+		</main>
+	);
 }
--- src/context/web3-provider.tsx
+++ src/context/web3-provider.tsx
@@ -1,77 +1,84 @@
 "use client";
-import { createWeb3Modal } from '@web3modal/wagmi/react'
-import { walletConnectProvider, EIP6963Connector } from '@web3modal/wagmi'
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { walletConnectProvider, EIP6963Connector } from "@web3modal/wagmi";
 
-import { WagmiConfig, configureChains, createConfig } from 'wagmi'
-import { publicProvider } from 'wagmi/providers/public'
-import { infuraProvider } from 'wagmi/providers/infura'
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { infuraProvider } from "wagmi/providers/infura";
 
-import { optimismGoerli, sepolia, arbitrumGoerli, linea, scroll, base } from 'viem/chains'
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
-import { ReactNode } from 'react';
+import { optimismGoerli, sepolia, arbitrumGoerli, linea, scroll, base, blastSepolia } from "viem/chains";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { ReactNode } from "react";
 
-const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!
-const infuraApiKey = process.env.NEXT_PUBLIC_INFURA_API_KEY!
-const scrollApiKey = process.env.NEXT_PUBLIC_BLOCKPI_SCROLL_API_KEY!
-const baseApiKey = process.env.NEXT_PUBLIC_BLOCKPI_BASE_API_KEY!
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!;
+const infuraApiKey = process.env.NEXT_PUBLIC_INFURA_API_KEY!;
+const scrollApiKey = process.env.NEXT_PUBLIC_BLOCKPI_SCROLL_API_KEY!;
+const baseApiKey = process.env.NEXT_PUBLIC_BLOCKPI_BASE_API_KEY!;
+const blastApiKey = process.env.NEXT_PUBLIC_BLAST_SEPOLIA_API_KEY!;
 
 const { chains, publicClient } = configureChains(
-  [ 
-    // optimismGoerli, 
-    // arbitrumGoerli, 
-    // sepolia, 
-    base,
-    linea, 
-    scroll, 
-  ],
-  [ 
-    publicProvider(),
-    jsonRpcProvider({
-      rpc: (chain) => ({
-        http: `https://scroll.blockpi.network/v1/rpc/${scrollApiKey}`,
-      }),
-    }),
-    jsonRpcProvider({
-      rpc: (chain) => ({
-        http: `https://base.blockpi.network/v1/rpc/${baseApiKey}`,
-      }),
-    }),
-    infuraProvider({ apiKey: infuraApiKey }),
-    walletConnectProvider({ projectId }), 
-  ]
-)
+	[
+		// optimismGoerli,
+		// arbitrumGoerli,
+		// sepolia,
+		blastSepolia,
+		base,
+		linea,
+		scroll,
+	],
+	[
+		publicProvider(),
+		jsonRpcProvider({
+			rpc: (chain) => ({
+				http: `https://rpc.ankr.com/blast_testnet_sepolia/${blastApiKey}`,
+			}),
+		}),
+		jsonRpcProvider({
+			rpc: (chain) => ({
+				http: `https://scroll.blockpi.network/v1/rpc/${scrollApiKey}`,
+			}),
+		}),
+		jsonRpcProvider({
+			rpc: (chain) => ({
+				http: `https://base.blockpi.network/v1/rpc/${baseApiKey}`,
+			}),
+		}),
+		infuraProvider({ apiKey: infuraApiKey }),
+		walletConnectProvider({ projectId }),
+	]
+);
 
 const metadata = {
-  name: 'OpenChat',
-  description: 'OpenChat App',
-  url: 'http://185.244.180.18:3000',
-  icons: ['https://avatars.githubusercontent.com/u/37784886']
-}
+	name: "OpenChat",
+	description: "OpenChat App",
+	url: "http://185.244.180.18:3000",
+	icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
 
 const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: [
-    new WalletConnectConnector({ chains, options: { projectId, showQrModal: false, metadata } }),
-    new InjectedConnector({ chains, options: { shimDisconnect: true } }),
-    new CoinbaseWalletConnector({ chains, options: { appName: metadata.name } })
-  ],
-  publicClient
-})
+	autoConnect: true,
+	connectors: [
+		new WalletConnectConnector({ chains, options: { projectId, showQrModal: false, metadata } }),
+		new InjectedConnector({ chains, options: { shimDisconnect: true } }),
+		new CoinbaseWalletConnector({ chains, options: { appName: metadata.name } }),
+	],
+	publicClient,
+});
 
 // 3. Create modal
-createWeb3Modal({ 
-  wagmiConfig, 
-  projectId, 
-  chains, 
-  themeMode: 'light',
-  themeVariables: {
-    '--w3m-border-radius-master': '1px'
-  }
-})
+createWeb3Modal({
+	wagmiConfig,
+	projectId,
+	chains,
+	themeMode: "light",
+	themeVariables: {
+		"--w3m-border-radius-master": "1px",
+	},
+});
 
 export function Web3Modal({ children }: { children: ReactNode }) {
-  return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
+	return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
 }
